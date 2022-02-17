7ad49dd688738add0253948e5f95dc1e9ca972ef
batukav <batuhankav@gmail.com>
2022-02-17 12:08:45 +0100

    initial upload

---
+++ Results/mc3_1_popc_19/slipids/conversion.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+head -n 2 em.gro
+
+export resname="POPC"
+
+
+for i in {1..50}
+do
+	for atom in $(cat POPC.itp);
+	do
+		string1="$i$resname" 
+		string2="$atom"
+		grep -e "\b$string1" em.gro | grep -e "\b$string2\b"
+	done
+done
+
+
+export resname="DMD"
+
+
+for i in {39..40}
+do
+	for atom in $(cat DMD.itp);
+	do
+		string1="$i$resname" 
+		string2="$atom"
+		
+		atomcharmm=`grep -e "DMD$atom\b" dmd_map.txt | awk '{print $2}'`
+		#echo $atomcharmm
+
+		grep -e "\b$string1" em.gro | grep -e "\b$atomcharmm\b" | awk '{print substr($0,1,10) v substr($0,15)}' v=$atom
+
+	done
+done
+
+
+export resname="TIP3"
+
+for i in {1..1500}
+do
+	string1="$i$resname" 
+	grep -e "\b$string1" em.gro
+	
+done
+
+tail -n 1 em.gro
---
+++ Results/mc3_3_dopc_17/slipids/conversion.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+head -n 2 em.gro
+
+export resname="DOPC"
+
+
+for i in {1..50}
+do
+	for atom in $(cat DOPC.itp);
+	do
+		string1="$i$resname" 
+		string2="$atom"
+		grep -e "\b$string1" em.gro | grep -e "\b$string2\b"
+	done
+done
+
+export resname="DMD"
+
+
+for i in {30..40}
+do
+        for atom in $(cat DMD.itp);
+        do
+                string1="$i$resname"
+                string2="$atom"
+
+                atomcharmm=`grep -e "DMD$atom\b" dmd_map.txt | awk '{print $2}'`
+                #echo $atomcharmm
+
+                grep -e "\b$string1" em.gro | grep -e "\b$atomcharmm\b" | awk '{print substr($0,1,10) v substr($0,15)}' v=$atom
+
+        done
+done
+
+
+export resname="TIP3"
+
+for i in {1..1500}
+do
+	string1="$i$resname" 
+	grep -e "\b$string1" em.gro
+	
+done
+
+tail -n 1 em.gro
